e8522c1373749eee7a1cd46cebf4ceef51d15808
mattyewen <84802457+mattyewen@users.noreply.github.com>
2021-10-09 18:16:28 -0400

    second commmit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -6,8 +6,8 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
  * @author Ronak
  */
 public class CardTrick {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+        Card card = new Card();
        
     }
     
